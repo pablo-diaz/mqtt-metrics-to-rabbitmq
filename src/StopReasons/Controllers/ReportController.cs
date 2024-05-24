4ba13becaf4ec68f882b8786b646b3b88f3eaa3a
Pablo Diaz <pablo@YUXMED0035L>
2024-05-24 11:47:38 -0500

    Improving UI

--- src/StopReasons/Controllers/ReportController.cs
+++ src/StopReasons/Controllers/ReportController.cs
@@ -20,10 +20,13 @@ public class ReportController: ControllerBase
 
     [HttpGet("downtimePeriods")]
     public IActionResult GetDowntimePeriods() =>
-        Content(content: $"{GetCsvHeader()}\n{GetCsvRowsForDowntimePeriods()}", contentType: "text/csv");
+        Content(content: $"{GetCsvHeader()}\n{GetDummyEntry()}\n{GetCsvRowsForDowntimePeriods()}", contentType: "text/csv");
 
     private static string GetCsvHeader() =>
         "_time,device_id,downtime_reason";
+
+    private static string GetDummyEntry() =>
+        "2020-01-01T00:00:01.000000000Z,NoDev,NoReason";  // this is used, so that Flux does not break on empty CSV result sets
 
     private string GetCsvRowsForDowntimePeriods() =>
         string.Join(separator: "\n",
